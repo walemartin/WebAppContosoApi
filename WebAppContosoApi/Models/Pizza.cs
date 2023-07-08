4d4fbd824a9be9bb9ce0b11b44486a38c257e792
Olasupo Olawale Martin <rytesoft@hotmail.com>
2023-07-08 14:53:50 +0200

    Add project files.

---
+++ WebAppContosoApi/Models/Pizza.cs
@@ -0,0 +1,9 @@
+﻿namespace WebAppContosoApi.Models
+{
+    public class Pizza
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public bool IsGlutenFree { get; set; }
+    }
+}
---
+++ WebAppContosoApi/PizzaService.cs
@@ -0,0 +1,44 @@
+﻿using System.Xml.Linq;
+using WebAppContosoApi.Models;
+
+namespace WebAppContosoApi
+{
+    public static class PizzaService
+    {
+        static List<Pizza> Pizzas { get; }
+        static int nextId = 3;
+        static PizzaService()
+        {
+            Pizzas = new List<Pizza>
+            {
+                new Pizza { Id = 1, Name = "Classic Nigerian Pizza", IsGlutenFree = false },
+                new Pizza { Id = 2, Name = "Dodo Pizza", IsGlutenFree = true },
+                new Pizza { Id = 3, Name = "Texas Dodo Pizza", IsGlutenFree = false },
+            };
+        }
+        public static List<Pizza> GetPizzas() => Pizzas;
+        public static Pizza? Get(int id) => Pizzas.FirstOrDefault(p => p.Id == id);
+        public static void Add(Pizza pizza)
+        {
+            pizza.Id = nextId++;
+            Pizzas.Add(pizza);
+        }
+        public static bool Delete(int id)
+        {
+            var pizza = Get(id);
+            return (pizza != null) ? Pizzas.Remove(pizza) : false;
+        }
+        public static void Update(Pizza pizza)
+        {
+            var index = Pizzas.FindIndex(p => p.Id == pizza.Id);
+            if (index != -1)
+            {
+                return;
+            }
+            else
+            {
+                Pizzas[index] = pizza;
+            }
+        }
+    }
+}
